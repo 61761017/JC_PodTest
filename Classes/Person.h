1565ae0e6c3e8d34995829bde6fa78ebeda844ef
井超 <61761017@qq.com>
2018-04-05 16:32:33 +0800

    1111

---
+++ Classes/Person.h
@@ -0,0 +1,15 @@
+//
+//  Person.h
+//  revise
+//
+//  Created by 井超 on 2018/3/29.
+//  Copyright © 2018年 井超. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@property (nonatomic, copy) NSString *name;
+
+@end
---
+++ Classes/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  revise
+//
+//  Created by 井超 on 2018/3/29.
+//  Copyright © 2018年 井超. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
